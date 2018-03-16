5fb0fd8b6c79c4df6625dc233addf3c6697cbfbb
yuki <yuki@shimizu-no-MacBook-Pro.local>
2018-03-16 14:55:05 +0900

    make chat-side outline

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,5 @@
 class MessagesController < ApplicationController
+  def index
+    @groups = [ { group_name: 'group1', group_last_message: 'おはよう' }, { group_name: 'group2', group_last_message: 'こんにちは' }]
+  end
 end
