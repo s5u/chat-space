70893f942578b629ac172bd305679d762dd1bdef
s5u <s5u@users.noreply.github.com>
2018-03-19 21:12:41 +0900

    Revert "Make chat view"

--- app/controllers/messages_controller.rb
+++
@@ -1,6 +0,0 @@
-class MessagesController < ApplicationController
-  def index
-    @groups = [ { group_name: 'group1', group_last_message: 'おはよう' }, { group_name: 'group2', group_last_message: 'こんにちは' }]
-    @messages = [ { name: 'yuki', message: 'おはよう' }, { name: 'yuji', message: 'こんにちは' }]
-  end
-end
